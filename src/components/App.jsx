711157fe1ff97ef6163a579a3b66f7957fd090c1
Andrey Kustov <kustovforwork@gmail.com>
2018-08-29 21:39:00 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { sort } from 'actions';
-import * as profiles from 'profiles';
 import './App.css'; 
 
 class App extends Component {
 
-  sort = (a, b, sortType, direction) => {
-    const arg_a = a[sortType].toLowerCase().split(/\(|\)/).join('');
-    const arg_b = b[sortType].toLowerCase().split(/\(|\)/).join('');
-    if (arg_a < arg_b) {
-      return direction ? -1 : 1;
-    }
-    if (arg_a > arg_b) {
-      return direction ? 1 : -1;
-    }
-
-    return 0;
-  }
-
   render() {
-    const { 
-      onSort,
-      sortType
-    } = this.props;
-    const direction = this.props.direction === null ? true : this.props.direction;
+    const { onSort, profiles } = this.props;
 
     return (
       <table className="table">
@@ -43,7 +25,6 @@ class App extends Component {
             ))}
           </tr>
           {profiles
-            .sort((a, b) => this.sort(a, b, sortType, direction))
             .map((it, ndx) => {
               return (
                 <tr key={ndx + new Date().getTime()}>
@@ -61,14 +42,13 @@ class App extends Component {
 }
 
 const mapStateToProps = state => ({
+  profiles: state.profiles,
   sortType: state.sortType, 
   direction: state.direction
 });
 
 const mapDispatchToProps = dispatch => ({
   onSort: ({ type, direction}) => {
-    localStorage.setItem('direction', direction);
-    localStorage.setItem('sortType', type);
     dispatch(sort({ type, direction }))
   }
 });
--- src/reducers.js
+++ src/reducers.js
@@ -1,6 +1,31 @@
 import { SORT } from 'types';
+import * as profiles from 'profiles';
+
+const sort = (a, b, sortType, direction) => {
+  localStorage.setItem('direction', direction);
+  localStorage.setItem('sortType', sortType);
+
+  const arg_a = a[sortType].toLowerCase().split(/\(|\)/).join('');
+  const arg_b = b[sortType].toLowerCase().split(/\(|\)/).join('');
+  if (arg_a < arg_b) {
+    return direction ? -1 : 1;
+  }
+  if (arg_a > arg_b) {
+    return direction ? 1 : -1;
+  }
+
+  return 0;
+}
+
+const profiles_toState = profiles.sort((a, b) => sort(
+    a, 
+    b, 
+    localStorage.getItem('sortType') || 'Name', 
+    JSON.parse(localStorage.getItem('direction')) ? JSON.parse(localStorage.getItem('direction')) : true
+));
 
 const initialState = {
+  profiles: profiles_toState,
   sortType: localStorage.getItem('sortType') || 'Name',
   direction: JSON.parse(localStorage.getItem('direction'))
 };
@@ -10,6 +35,7 @@ export default (state = initialState, action) => {
     case SORT:
       return {
         ...state,
+        profiles: profiles.sort((a, b) => sort(a, b, action.payload.type, action.payload.direction)),
         sortType: action.payload.type,
         direction: action.payload.direction, 
       };
